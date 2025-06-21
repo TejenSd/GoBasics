86f302429aa986c81caeb31afef5234bd78b31ec
Tejendra Singh Danu <Tejenderdanu46@gmail.com>
2025-06-21 12:33:17 +0530

    refactor for.go: clean up comments and improve loop structure

--- 5_For/for.go
+++ 5_For/for.go
@@ -11,10 +11,31 @@ func main() {
 		i = i + 1
 	}
 
-	//Infinite loop using for
-	for {
-		fmt.Println("This is an infinite loop")
+	// //Infinite loop using for
+	// for {
+	// 	fmt.Println("This is an infinite loop")
 
+	// }
+
+	for i = 1; i <= 3; i++ {
+
+		if i == 2 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	// for loop witth range
+	for i, v := range []int{1, 2, 3} {
+		fmt.Println("Index:", i, "Value:", v)
+	}
+
+	for i := range 11 {
+		if condition := i % 2; condition == 0 {
+			continue
+			
+		}
+		fmt.Println(i)
 	}
 
 }
